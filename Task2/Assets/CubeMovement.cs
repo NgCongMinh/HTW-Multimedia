58923cc7a75a08f47f5f94017c7eec1ce068b79c
Unknown <ngcongminh.dev@gmail.com>
2019-04-21 22:11:46 +0200

    task 2

---
+++ Task2/Assets/CubeMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        registerKeyListener();
+    }
+
+    private void registerKeyListener()
+    {
+        var move = new Vector3(
+            Input.GetAxis("Horizontal"), 
+            Input.GetAxis("Vertical"), 
+            0
+            );
+        transform.position += move * speed * Time.deltaTime;
+    }
+
+}
---
+++ Task2/Assets/Follower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = target.position;
+        transform.LookAt(target);
+        transform.position = new Vector3(
+            pos.x + offset, 
+            pos.y + offset, 
+            pos.z + offset
+            );
+    }
+}
