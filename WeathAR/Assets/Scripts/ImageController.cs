7232e3aaad06fdffce25db9092e3f9b4d6c4c36e
Cong Minh Nguyen <ngcongminh.dev@gmail.com>
2019-06-11 01:18:18 +0200

    add marker to project

---
+++ WeathAR/Assets/Scripts/ImageController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class ImageController : MonoBehaviour, ITrackableEventHandler
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        Debug.Log("HALLO");
+        if (newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            Debug.Log("TRACKED " );
+        }
+        else if (newStatus == TrackableBehaviour.Status.DETECTED)
+        {
+            Debug.Log("DETECTED " );
+        }
+    }
+}
