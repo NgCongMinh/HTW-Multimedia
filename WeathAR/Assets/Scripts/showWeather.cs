b6655ec8605c0538de36b5703cf8bd8df2a3ab0b
Tung Tuan Tran <licki_tung@hotmail.de>
2019-07-07 12:12:29 +0200

    add tracker

---
+++ WeathAR/Assets/Scripts/showWeather.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showWeather : MonoBehaviour
+{
+    private GameObject andereWetterdaten;     //vorherhige Wetterdaten / von anderem bundesland
+	private GameObject wetterdaten;
+	//private bool selected = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        andereWetterdaten = null;
+        wetterdaten = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(andereWetterdaten != null){
+            andereWetterdaten.SetActive(false);
+        }
+        andereWetterdaten = wetterdaten;
+        wetterdaten.SetActive(true);
+  
+    }
+
+    void OnTriggerEnter(Collider col){
+    	if(col.tag == "Marker"){
+            wetterdaten = col.transform.Find("Wetterdaten").gameObject;
+    		//selected = true;
+    	}
+
+    }
+
+    /*
+    void OnTriggerExit(Collider col){
+    	if(col.tag == "Marker"){
+    		selected = false;
+
+    		Debug.Log("y");
+    	}
+
+    }
+    */
+}
