0409c0cb324cb6385d057267d6735366d19647aa
Cong Minh Nguyen <ngcongminh.dev@gmail.com>
2019-07-20 20:55:44 +0200

    temperature toggle script

--- WeathAR/Assets/Scripts/Menu/Menu.cs
+++ WeathAR/Assets/Scripts/Menu/Menu.cs
@@ -1,19 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Menu : MonoBehaviour
+
+namespace Menu
 {
-	
-	public void OpenFAQ(){
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-	}
+    public class Menu : MonoBehaviour
+    {
+        public void OpenFAQ()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
 
-	public void OpenTemp(){
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
-	}
+        public void OpenTemp()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        }
 
-	public void OpenMain(){
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-	}
+        public void OpenMain()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
 }
